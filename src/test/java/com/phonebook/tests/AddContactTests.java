4038824036658762afd6e1cc8bf43b7d9eee7574
Juri Ryzhkov <plktmn@gmail.com>
2024-03-04 10:49:06 +0100

    Added Lesson 8

--- src/test/java/com/phonebook/tests/AddContactTests.java
+++ src/test/java/com/phonebook/tests/AddContactTests.java
@@ -5,7 +5,16 @@ import com.phoneBook.models.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class AddContactTests extends TestBase{
     //precondition
@@ -40,6 +49,59 @@ public class AddContactTests extends TestBase{
     @AfterMethod
    public void postCondition(){
         app.getContact().removeContact();
+    }
+
+    @DataProvider
+    public Iterator<Object[]>addContact(){
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{"oliver", "Kan", "12345678902", "kan@mail.com", "Berlin", "User"});
+        list.add(new Object[]{"oliver1", "Kan", "12345678902", "kan1@mail.com", "Berlin", "User"});
+        list.add(new Object[]{"oliver2", "Kan", "12345678902", "kan2@mail.com", "Berlin", "User"});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "addContact")
+    public void addContactPositiveTestFromDataProvider(String name, String lastName, String phone, String email
+    , String address, String desc){
+        app.getContact().clickOnAddLink();
+        app.getContact().fillContactForm(
+                new Contact()
+                        .setName(name)
+                        .setLastName(lastName)
+                        .setPhone(phone)
+                        .setEmail(email)
+                        .setAddress(address)
+                        .setDescription(desc));
+
+        app.getContact().clickOnSaveButton();
+        Assert.assertTrue(app.getContact().isContactCreateByText(name));
+    }
+
+    @DataProvider
+    public Iterator<Object[]> addContactFromCSV() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
+        String line = reader.readLine();
+
+        while (line != null) {
+
+            String[] split = line.split(",");
+
+            list.add(new Object[]{new Contact().setName(split[0]).setLastName(split[1]).setPhone(split[2])
+                    .setEmail(split[3]).setAddress(split[4]).setDescription(split[5])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "addContactFromCSV")
+    public void addContactPositiveTestFromDataProviderWithFile(Contact contact){
+        app.getContact().clickOnAddLink();
+        app.getContact().fillContactForm(contact);
+
+        app.getContact().clickOnSaveButton();
+        Assert.assertTrue(app.getContact().isContactCreateByText(contact.getName()));
     }
 
 }
--- src/test/java/com/phonebook/tests/TestBase.java
+++ src/test/java/com/phonebook/tests/TestBase.java
@@ -2,12 +2,22 @@ package com.phonebook.tests;
 
 import com.phoneBook.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
     //    @BeforeMethod
     @BeforeSuite
@@ -19,6 +29,23 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start test" + method.getName());
+    }
+    @AfterMethod
+    public void stopTest(ITestResult result){
+        if (result.isSuccess()){
+            logger.info("Passed" + result.getMethod().getMethodName());
+        } else {
+            logger.error("Failed" + result.getMethod().getMethodName()
+                    + "Screenshot -> " + app.getUser().takeScreenshot());
+        }
+        logger.info("Stop test");
+        logger.info("_____________________________________________");
+
     }
 
 }
